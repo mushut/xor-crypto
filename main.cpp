e38b37db7a336aad78c97b315b21919842ede427
Aki Sipovaara <mushut@gmail.com>
2018-08-23 13:55:33 +0300

    More commenting

--- main.cpp
+++ main.cpp
@@ -25,13 +25,13 @@ int main(void)
 	std::cin >> key;
 	unsigned int bitwise_key = 0;
 	std::string::reverse_iterator ptr;
-	int counter = 0;
+	int key_length = 0;
 	for (ptr = key.rbegin(); ptr < key.rend(); ptr++) {
 		if (*ptr == '1') {
-			bitwise_key |= (1 << counter);
+			bitwise_key |= (1 << key_length);
 		}
  
-		counter++;
+		key_length++;
 	}
 
 	// Operation
@@ -43,11 +43,29 @@ int main(void)
 	
 	// Write to file
 	unsigned int tempKey;	// Temporary holder for 8-bit part of key
-	tempKey = bitwise_key;
+	int bit_masks[8];	// Used for creating 8-bit tempKey for encrytpion
+	for (int i = 0; i < 8; i++) {
+		bit_masks[i] = (1 << (7 - i));	// Mask variables start from most significant bit
+	}	
+
+	if (key_length < 8) {
+		// tempKey must be exactly 8 bits length, so continue bit values from bitwise_key
+		// TBD
+	}
+	if (key_length == 8) {
+		// tempKey doesn't change during encryption.
+		tempKey = bitwise_key;
+	}
+	if (key_length > 8) {
+		// tempKey must be exactly 8 bits length, so continue bit values from bitwise_key
+		// TBD
+	}
 
 	char character;
 	char result;
 	while (input_file.get(character)) {
+		// tempKey must be changed if key_length is not exactly 8 bits long
+		// for every iteration.
 		result = character ^ tempKey;
 
 		output_file.put(result);
